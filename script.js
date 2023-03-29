bab4af7229dbda8f207bb2bd3702b864312aadfe
kalpna-hub <122824448+kalpna-hub@users.noreply.github.com>
2023-03-29 20:47:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-//your JS code here. If required.
+//your JS code here.
+If required.
 var nameElement = document.querySelector("#name");
 var ageElement = document.querySelector("#age");
 var btnElement = document.querySelector("#btn");
