b062b45a3263c3b68d3f7b4c0dbb3fc1e4bcad66
kalpna-hub <122824448+kalpna-hub@users.noreply.github.com>
2023-04-14 11:05:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,24 @@
-//your JS code here.
-If required.
-var nameElement = document.querySelector("#name");
-var ageElement = document.querySelector("#age");
-var btnElement = document.querySelector("#btn");
+const nameInput = document.getElementById('name');
+const ageInput = document.getElementById('age');
+const submitBtn = document.getElementById('btn');
 
-var promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    var age = ageElement.value;
-    var name = nameElement.value;
-   var age = ageElement.value;
-    var name = nameElement.value;
-   if (!(age && name)) {
-     alert("Please enter valid details");
-   }
-    return;
-    if (age < 18) {
-      reject(`Oh sorry ${name}. You aren't old enough`);
-    } else {
-      resolve(`Welcome, ${name}. You can vote.`);
-    }
-  }, 4000);
+submitBtn.addEventListener('click', (event) => {
+  event.preventDefault(); 
+  const delayPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Number(ageInput.value) >= 18) {
+        resolve({ name: nameInput.value, age: Number(ageInput.value) });
+      } else {
+        reject({ name: nameInput.value });
+      }
+    }, 4000);
+  });
+
+  delayPromise
+    .then((data) => {
+      alert(`Welcome, ${data.name}. You can vote.`);
+    })
+    .catch((error) => {
+      alert(`Oh sorry ${error.name}. You aren't old enough.`);
+    });
 });
-
-function callPromise() {
-    return promise;
-}
-function handleSubmit(e) {
-    e.preventDefault();
-  callPromise().then(alert).catch(alert);
-}
-
-btnElement.addEventListener("click", handleSubmit);
